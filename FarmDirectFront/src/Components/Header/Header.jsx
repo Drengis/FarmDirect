7a56a07d279923571e06b4bf5b8d8599f4d4d3ae
Drengis <lun3218@gmail.com>
2024-12-23 17:40:11 +0500

    Create Side for Header buttons

--- FarmDirectFront/src/Components/Header/Header.jsx
+++ FarmDirectFront/src/Components/Header/Header.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../Button/Button';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import Sidebar from '../SideBar/SideBar';
 
 const Header = () => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const props = [
+        { name: 'Главная', path: '/' },
+        { name: 'Препараты', path: '/medicine' },
+    ];
+
+    const checkWindowSize = () => {
+        if (window.innerWidth < 600) {
+            setIsMenuVisible(true);
+        } else {
+            setIsMenuVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        checkWindowSize();
+        window.addEventListener('resize', checkWindowSize);
+
+        return () => {
+            window.removeEventListener('resize', checkWindowSize);
+        };
+    }, []);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div className="header">
-            <div className='Link_buttons'>
-                <Link to='/'><Button name='Домашняя старница' /></Link>
-                <Link to='/medicine'><Button name='Препараты' /></Link>
-            </div>
+            {isMenuVisible ? (
+                <div className='menu-button'>
+                    <Button name='Меню' onClick={toggleSidebar} />
+                </div>
+            ) : (
+                <div className='Link_buttons'>
+                    <Link to='/'><Button name='Главная' /></Link>
+                    <Link to='/medicine'><Button name='Препараты' /></Link>
+                </div>
+            )}
             <div className='LogoConteiner'>
-                <h2 className='Logo'> Farm-Direct </h2>
+                <h2 className='Logo'> Farm&nbsp;Direct </h2>
             </div>
+            <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} props={props} />
         </div>
     );
 };
---
+++ FarmDirectFront/src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '../Button/Button';
+import './Sidebar.css';
+import { Link } from 'react-router-dom';
+
+const Sidebar = ({ isOpen, toggleSidebar, props }) => {
+    return (
+        <div className={`Sidebar ${isOpen ? 'open' : ''}`}>
+            <div className="Close-btn-Conteiner">
+                <button className="Close-btn" onClick={toggleSidebar}>✖</button>
+            </div>
+            <div className='Sidebar-Item'>
+                {props.map((link, index) => (
+                    <Link to={link.path} key={index}>
+                        <Button
+                            name={link.name}
+                            onClick={() => {
+                                toggleSidebar();
+                            }}
+                        />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
