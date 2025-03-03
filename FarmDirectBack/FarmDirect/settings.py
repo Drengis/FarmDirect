b91ad757b3c26755eed098e799d56c63ccac8fc4
Drengis <lun3218@gmail.com>
2025-03-03 14:26:57 +0500

    connect db

--- FarmDirectBack/FarmDirect/settings.py
+++ FarmDirectBack/FarmDirect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from db_settings import HOST, NAME, USER, PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,15 @@ WSGI_APPLICATION = 'FarmDirect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': '3306',
     }
 }
 
